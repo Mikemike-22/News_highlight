1dfbaedc3f4b1be77022a7406790fc40a90bfcb4
Michael Korir <mikor22@gmail.com>
2019-05-23 16:20:44 +0300

    create view functionality for articles page

--- app/views.py
+++ app/views.py
@@ -7,5 +7,14 @@ def index():
     '''
     view root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = "Hello world"
+    return render_template('index.html', message = message)
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    '''
+    view page fundtion that returns the news details page and its data
+    '''
+    return render_template('article.html', id = article_id)
+
     
